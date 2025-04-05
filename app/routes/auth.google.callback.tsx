ad40d35b5ad4114884ecf407ddcd0a77700a9ed3
JUNYI OU <49964599+Irvingouj@users.noreply.github.com>
2025-04-05 11:03:01 -0400

    better failure page

--- app/routes/auth.google.callback.tsx
+++ app/routes/auth.google.callback.tsx
@@ -4,6 +4,6 @@ import { authenticator } from "common/auth/auth.server";
 export const loader: LoaderFunction = ({ request }) => {
   return authenticator.authenticate("google", request, {
     successRedirect: "/?startChoose=true",
-    failureRedirect: "/login",
+    failureRedirect: "/login/failure",
   });
 }; 
